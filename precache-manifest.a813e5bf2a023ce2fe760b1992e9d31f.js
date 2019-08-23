b0fb42ea916a7dd7a4ba70e3639a4bdc2e65cc83
Hal Helms <hal.helms@gmail.com>
2019-08-23 15:26:19 -0700

    Updates

---
+++ precache-manifest.a813e5bf2a023ce2fe760b1992e9d31f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2250a8c922ea84822d41bb3e01ec2e",
+    "url": "/codingcareer/index.html"
+  },
+  {
+    "revision": "4eeb6dfbe68c9a870c1d",
+    "url": "/codingcareer/static/css/main.90521a04.chunk.css"
+  },
+  {
+    "revision": "4d0d5f24c3673a4adaa7",
+    "url": "/codingcareer/static/js/2.9d915a1f.chunk.js"
+  },
+  {
+    "revision": "4eeb6dfbe68c9a870c1d",
+    "url": "/codingcareer/static/js/main.897f5dc9.chunk.js"
+  },
+  {
+    "revision": "11696e5bf8da58b36243",
+    "url": "/codingcareer/static/js/runtime~main.88cdfea6.js"
+  },
+  {
+    "revision": "f2e92d1ac887d19425824f454a327c1a",
+    "url": "/codingcareer/static/media/average-js-salary.f2e92d1a.jpg"
+  },
+  {
+    "revision": "cdd492b4d602c04cc0164a1a282d951f",
+    "url": "/codingcareer/static/media/current-jobs.cdd492b4.png"
+  },
+  {
+    "revision": "cac41a2f4afe186af3e73fed0cb572e7",
+    "url": "/codingcareer/static/media/logo.cac41a2f.png"
+  },
+  {
+    "revision": "2ce64539c6fa935c4c9e92a645fc1d52",
+    "url": "/codingcareer/static/media/moores-law.2ce64539.png"
+  },
+  {
+    "revision": "5ce48d004b45efacd35560f470db239a",
+    "url": "/codingcareer/static/media/noahs-ark.5ce48d00.png"
+  },
+  {
+    "revision": "44c30609924b91cfc98704a15b970e67",
+    "url": "/codingcareer/static/media/robot-girder-workers.44c30609.png"
+  },
+  {
+    "revision": "c000b2eb2a76cb578f7255af89652549",
+    "url": "/codingcareer/static/media/salaries-by-state.c000b2eb.png"
+  }
+]);
